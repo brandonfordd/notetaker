470559193729c0f0913aa24fd22909d8c8dd88be
Brandon Ford <Brandon.ford.617@gmail.com>
2021-04-26 16:01:37 -0400

    added static function to path

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//serve static files in the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 
